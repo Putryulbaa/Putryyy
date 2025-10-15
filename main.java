6e468df6061c3e9eef08a7b061ff37a9618d2ec1
Putryulbaa <2400018186@webmail.uad.ac.id>
2025-10-15 16:27:13 +0700

    Create main.java
    Karyawan pbo laprak 2

---
+++ main.java
@@ -0,0 +1,31 @@
+public class Karyawan {
+    private String nik;
+    private String nama;
+    private String jabatan;
+
+    public void setNik(String nik) {
+        this.nik = nik;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNik() {
+        return this.nik;
+    }
+
+    public String getNama() {
+        return this.nama;
+    }
+
+    public String getJabatan() {
+        return this.jabatan;
+    }
+
+
+    public void setJabatan(String jabatan) {
+        this.jabatan = jabatan;
+    }
+
+}
